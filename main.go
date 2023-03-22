661c7db9401684c462eb8d03db89c00cf21ea4df
Ved misra <47312748+misraved@users.noreply.github.com>
2023-03-23 01:38:27 +0530

    Recompile plugin with steampipe-plugin-sdk v5.3.0 (#3)

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/turbot/steampipe-plugin-ipinfo/ipinfo"
-	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 )
 
 func main() {

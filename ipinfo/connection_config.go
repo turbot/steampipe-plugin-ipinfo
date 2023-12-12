09e2d484df133675e43c01fbca6d7e20cd1f5ae1
Madhushree Ray <madhushree@turbot.com>
2023-12-12 21:13:29 +0530

    Remove config schema

--- ipinfo/connection_config.go
+++ ipinfo/connection_config.go
@@ -2,17 +2,10 @@ package ipinfo
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type ipinfoConfig struct {
-	Token        *string `cty:"token"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"token": {
-		Type: schema.TypeString,
-	},
+	Token        *string `hcl:"token"`
 }
 
 func ConfigInstance() interface{} {
